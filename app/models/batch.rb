fa56a480634efb3a00ec0f4846896f0dfb22de1e
Gillys Ayres <gillys.ayres@gmail.com>
2022-02-07 22:01:22 -0300

    erasing batch model

--- app/models/batch.rb
+++
@@ -1,9 +0,0 @@
-class Batch < ApplicationRecord
-  belongs_to :event
-
-  validates :price, :quantity, :name, presence: true
-
-  has_many :passes
-
-  scope :not_ended, -> { where(ended_at: nil) }
-end
