e7b7f57d258f6604658121cf44e11da7f08c52a8
Felipe Santos <santos.felipe13@gmail.com>
2022-03-08 18:12:24 -0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,7 +78,7 @@ end
 puts "-- OK!"
 
 puts 'Criando eventos...'
-1.times do 
+10.times do 
   scheduled_start = Faker::Date.between(from: Date.today, to: rand(5..15).days.from_now)
   event = Event.create!(name: Faker::BossaNova.song,
     description: (1..20).map { |i| Faker::TvShows::Suits.quote}.join(". "),
@@ -120,7 +120,7 @@ end
 
 puts 'Criando day uses...'
 
-1.times do
+3.times do
   day_use = DayUse.create!(name: Faker::WorldCup.stadium,
     description: (1..20).map { |i| Faker::TvShows::Suits.quote}.join(" "),
     partner_id: Partner.first.id,
--- db/seeds/states_and_cities_populate.rb
+++ db/seeds/states_and_cities_populate.rb
@@ -6,7 +6,7 @@ class StatesAndCitiesPopulate
     def populate!
       return if State.count.positive?
 
-      states.first(5).each do |state|
+      states.each do |state|
         state_obj = State.find_or_create_by!(
           acronym: state['acronym'],
           name:    state['name']
@@ -18,7 +18,7 @@ class StatesAndCitiesPopulate
     end
 
     def populate_cities(state, cities = [])
-      cities.first(10).each do |city|
+      cities.each do |city|
         city_obj = state.cities.find_or_create_by!(
           name: city['name']
         )
