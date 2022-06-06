daf6367f2fed8a4f1fca143e6df9fcc20012ea14
Felipe Santos <santos.felipe13@gmail.com>
2022-06-06 00:47:20 -0300

    Remove hard coded value

--- app/models/order.rb
+++ app/models/order.rb
@@ -50,7 +50,6 @@ class Order < ApplicationRecord
           number: "-",
         }
       },
-      customer_payment_method_id: "1B49A34E7581460FB9E77F757C78E992k",
       ignore_due_email: true,
       ignore_canceled_email: true,  
       due_date: (Time.current + 10.days),  
