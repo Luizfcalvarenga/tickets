eb83b77a4063aa0f188b606297cc04e13c861c25
Gillys <70576668+gillysayres@users.noreply.github.com>
2022-06-06 23:05:44 -0300

    semantical changes for event_url global variable

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,7 +4,7 @@ class EventMailer < ApplicationMailer
   def send_event_communication(user, event_communication)
     @user = user
     @event_communication = event_communication
-    @event_url = event_event_communication_url(event_id: @event_communication.event_id, id: @event_communication.id)
+    @event_communication_url = event_event_communication_url(event_id: @event_communication.event_id, id: @event_communication.id)
 
     event_communication.files.each do |file|
       attachments[file.filename.to_s] = file.download
