e40d02983bc5651aff1ad044e29300b454f5089c
Felipe Santos <santos.felipe13@gmail.com>
2022-07-08 17:42:24 -0300

    Increase days check amount from 1 to 7

--- app/jobs/payment_checker_job.rb
+++ app/jobs/payment_checker_job.rb
@@ -2,7 +2,7 @@ class PaymentCheckerJob < ApplicationJob
   queue_as :default
 
   def perform
-    Order.where("created_at > ?", Time.current - 1.day).where(invoice_status: "pending").each do |order|
+    Order.where("created_at > ?", Time.current - 7.day).where(invoice_status: "pending").each do |order|
       ::NovaIugu::ChargeCheckAndUpdateStatus.new(order).call
     end
   end
