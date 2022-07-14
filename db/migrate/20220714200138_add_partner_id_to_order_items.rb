23290bd8ae55fb2462c73b0e28f7f634c8980829
Felipe Santos <santos.felipe13@gmail.com>
2022-07-14 17:27:52 -0300

    Add up/down to migration

--- db/migrate/20220714200138_add_partner_id_to_order_items.rb
+++ db/migrate/20220714200138_add_partner_id_to_order_items.rb
@@ -1,9 +1,13 @@
 class AddPartnerIdToOrderItems < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_reference :order_items, :partner, foreign_key: true
-
+    
     OrderItem.find_each do |order_item|
       order_item.update(partner: order_item.related_entity.partner)
     end
+  end
+  
+  def down
+    remove_reference :order_items, :partner
   end
 end
