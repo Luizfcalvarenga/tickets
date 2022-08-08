77432bcbf8d292c7fbcdf8f8092f5c789010daa6
Juliano Torres <juliano@torresetorres.com>
2022-08-08 08:00:26 -0300

    Add another bot on discord

--- app/libs/discord_messager.rb
+++ app/libs/discord_messager.rb
@@ -1,11 +1,13 @@
 class DiscordMessager
-  WEBHOOK_URL = "https://discord.com/api/webhooks/959197791710756954/BaYV1XQvts0CVukiogNtsP-tdPfwl9z9zriSiKzSnvwc222W2naksXhfYFl3mJauUM2W"
+  NOVAMENTE_WEBHOOK_URL = "https://discord.com/api/webhooks/959197791710756954/BaYV1XQvts0CVukiogNtsP-tdPfwl9z9zriSiKzSnvwc222W2naksXhfYFl3mJauUM2W"
+  NUFLOW_WEBHOOK_URL = "https://discord.com/api/webhooks/1006153719718617211/anR2vpuSkv67MbnjJrGv9DZ0YAGKLtklpSbXjaxbrB1i16vMIiBMD_ujU1ov8nGkt6uY"
 
   def call(content)
     return true unless Rails.env.production?
 
     begin
-      HTTParty.post(WEBHOOK_URL, body: { content: content })
+      HTTParty.post(NOVAMENTE_WEBHOOK_URL, body: { content: content })
+      HTTParty.post(NUFLOW_WEBHOOK_URL, body: { content: content })
     rescue
       false
     end
