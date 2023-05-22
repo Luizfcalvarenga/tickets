35c0f02424517bd8c66a5fb0e0e907fa2b3ffc80
Luiz Alvarenga <luizfca7@gmail.com>
2023-05-22 10:21:51 -0300

    iteration to fill each ticket with user infos from his choice

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -6,7 +6,6 @@ class QuestionAnswersController < ApplicationController
     @order = Order.find(params[:order_id])
     @order_items = @order.order_items
     @question_answer = QuestionAnswer.new
-    # @selected_tickets = params[:tickets].map { |ticket| JSON.parse(ticket) } if params[:tickets]
     @user = current_user
   end
 
